d4acdf442e3d612844ced3b03748b7d1881433cc
Krishan singh <166697671+krishan-coder@users.noreply.github.com>
2025-02-11 15:08:45 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // it is used in browser not node.js
+// alert(3+2)
+
+let name = "Krishan"
+let age = 21
+let isTrue = false
+
+// number 2 to power 53
+// bigint 
+// String
+// boolean
+// null -> standalone value (defined as empty value) or non value
+// undefined 
+ // symbol 
+
+
+
+ // objects
+
+ console.log(typeof "krish");
+ console.log(typeof null); // treated as object (could be language error)
+ 
