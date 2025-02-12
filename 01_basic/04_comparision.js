06a297db8b36e33fd0ee6bc3e676795a165f5b35
Krishan singh <166697671+krishan-coder@users.noreply.github.com>
2025-02-12 15:20:30 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);  // true
+// console.log("02" > 1); // true
+
+// console.log(null >0); //false
+// console.log(null ==0); // false
+// console.log(null >=0); // true  comparision convert null to zero(0);
+
+// console.log(undefined > 0); //false
+// console.log(undefined ==0); // false
+// console.log(undefined >=0); // false  
+
+// === Strict check
+
+console.log("2" === 2); // false
+console.log(true === 1); // false
+
+// there must be no comparision between the different data types.
+
