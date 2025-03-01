2c0732e224b28bd07af44ac653168445cf921119
Krishan singh <166697671+krishan-coder@users.noreply.github.com>
2025-03-01 08:12:10 +0000

    Date_added

--- 09_project3/DIGITAL_Clock.js
+++ 09_project3/DIGITAL_Clock.js
@@ -1,7 +1,12 @@
 // const clock = document.querySelector("#clock");
 const clock = document.getElementById('clock');
+const date = document.querySelector("#date");
 
 setInterval(function(){
     let date = new Date();
     clock.innerHTML = date.toLocaleTimeString();
+},1000);
+setInterval(function(){
+    let d = new Date();
+    date.innerHTML = d.toLocaleDateString();
 },1000);
