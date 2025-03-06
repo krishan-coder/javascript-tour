f3c5e34e00f7ad74024289c67414fd9ab78d106a
Krishan singh <166697671+krishan-coder@users.noreply.github.com>
2025-03-06 13:15:14 +0000

    key-detector

---
+++ 12_keyboard/key.js
@@ -0,0 +1,21 @@
+const insert = document.querySelector('.insert');
+
+    window.addEventListener('keydown',(e) =>{
+        insert.innerHTML = 
+        `<div class="color">
+        <table border="1">
+            <tr>
+            <th>Key</th>
+            <th>Keycode</th> 
+            <th>Code</th>
+            </tr>
+            <tr>
+            <td>${e.key === ' ' ? 'Space' : e.key}</td>
+            <td>${e.which || e.keyCode}</td> 
+            <td>${e.code}</td>
+            </tr>
+            
+        </table>
+        </div>
+  `;
+});
